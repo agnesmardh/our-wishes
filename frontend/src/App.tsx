872c8819b26c2177a42df89a025dcc43896fc38a
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2021-04-03 08:17:30 +0200

    Add environment variables for cognito

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { LoginContainer } from './login/LoginContainer';
 import { Amplify } from 'aws-amplify';
-import awsConfig from './config/aws-config';
 
 Amplify.configure({
-  aws_cognito_region: awsConfig.cognito.REGION,
-  aws_user_pools_id: awsConfig.cognito.USER_POOL_ID,
-  aws_user_pools_web_client_id: awsConfig.cognito.APP_CLIENT_ID
+  aws_cognito_region: process.env.REACT_APP_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_COGNITO_USER_POOL_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_COGNITO_APP_CLIENT_ID
 });
 
 export const App: React.FC = () => <LoginContainer />;
