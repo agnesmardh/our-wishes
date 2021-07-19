321f5697731157743d1965ed6b53af20bbf6fa7e
agnesmardh <agnes.mardh@gmail.com>
2021-07-19 11:53:51 +0200

    Add user controller

---
+++ backend/backend/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using System.Threading.Tasks;
+using backend.models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    
+    public class UserController : BaseController
+    {
+        public UserController(WishlistContext context) : base(context)
+        {
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> CreateUser([FromBody] UserDto.CreateUserDto createUserDto)
+        {
+            var user = new User()
+            {
+                Email = createUserDto.Email,
+                Username = createUserDto.Username,
+                UserId = createUserDto.Id
+            };
+            
+            _context.Users.Add(user);
+
+            await _context.SaveChangesAsync();
+            
+            return UserDto.ToDto(user);
+        }
+        
+        [HttpGet("{id}")]
+        public ActionResult<UserDto> GetUser(string id)
+        {
+            var user = _context.Users.First(u => u.UserId == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return UserDto.ToDto(user);
+        }
+    }
+}
